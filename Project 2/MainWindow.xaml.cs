e81480e68e363418137f1f0f9419d1b24d540140
nwushawon <nwushawon@gmail.com>
2019-09-09 23:49:53 +0600

    first commit

---
+++ Project 2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Diagnostics;
+using System.Windows.Navigation;
+
+using System.Windows.Threading;
+
+namespace Project_2_CSE_Summmer_14_Seb_B
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick+=timer_Tick;
+            timer.Start();
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            txt_clock.Text = String.Format("{0:h:mm:ss tt}",DateTime.Now);
+        }
+
+
+        private void Hyperlink_RN(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
